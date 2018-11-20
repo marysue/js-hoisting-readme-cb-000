d457f4d5ce9965bb50c5dbf7131303fc41a92b00
Mary Lark <mary@marylark.com>
2018-11-20 18:33:35 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -3,28 +3,32 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  var lyric = "maybe";  //must set lyric before returning it.
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+
+  // fix the code in here:
+  //this call to the function must follow the assignment.  
+  //despite the var declaration being hoisted (var thisIsCrazy())
+  //the assignment is not hoisted.  So calling thisIsCrazy(); before
+  //it is assigned won't work
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
-
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
+
+  sayMy();
 }
